896cc595d06221af0e8c75b2a2dc2a71366a55e3
Hanzz <muhammadnabhanzaki2008@gmail.com>
2025-03-13 17:14:10 +0700

    skena1.php

--- skena1.php
+++ skena1.php
@@ -1,16 +1,18 @@
 <?php
-$nilai = 100;
+
+$nilai = 90;
+ 
 if ($nilai > 100) {
-   echo "Nilai $nilai = Nilai wajib di antara 0 - 100";
-} else if ($nilai >= 90) {
-    echo "Nilai $nilai = A";
-} else if ($nilai >= 80) {
-    echo "Niali $nilai = B";
-} else if ($nilai >= 70) {
-    echo "Nilai $nilai = C";
-} else if ($nilai >= 0) {
-    echo "Nilai $nilai = D";
+    echo "Nilai anda $nilai = Nilai wajib di antara 0 - 100";
+}elseif ($nilai >= 90) {
+    echo "Nilai anda $nilai = A";
+} else if ($nilai >= 80){
+    echo "Nilai anda $nilai = B ";
+} else if ($nilai >= 70){
+    echo "Nilai anda $nilai = C";
+} else if ($nilai >= 0 ){
+    echo "Nilai anda $nilai = D";
 } else {
-    echo "Nilai $nilai = Nilai wajib di antara 0 - 100";
+    echo "Nilai anda $nilai = Nilai wajib di antara 0 - 100";
 }
 ?>
